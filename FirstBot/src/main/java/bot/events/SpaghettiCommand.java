6c01d149fc20bc932a0ca51599a61c1f29b87e08
JakeMcAndrew221 <58837361+JakeMcAndrew221@users.noreply.github.com>
2020-03-09 12:12:53 +1100

    Add files via upload

---
+++ FirstBot/src/main/java/bot/events/SpaghettiCommand.java
@@ -0,0 +1,20 @@
+package bot.events;
+
+import java.io.File;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class SpaghettiCommand extends ListenerAdapter{
+
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent e)  {
+		String i = e.getMessage().getContentRaw();
+		
+		if (i.equalsIgnoreCase("spaghetti")) {
+			
+			File spag = new File("/Users/jake/Downloads/spag.mp4");
+			e.getChannel().sendFile(spag).queue();
+			
+		}	
+	}
+}
